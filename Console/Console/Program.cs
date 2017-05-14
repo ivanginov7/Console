04d6a9513144bd6ddc47e21ec9b2156cfdfcf67a
ivanginov7 <ivanginov7@gmail.com>
2017-05-14 09:06:52 +0300

    Changed program to number addition

--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Console
+namespace ConsoleProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <10; i++)
-            {
-                System.Console.WriteLine(i);
-            }
+            Console.WriteLine("Numbers addition");
+            Console.Write("Enter first digit: ");
+            var FirstNumber = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter second digit: ");
+            var SecondNumber = Convert.ToDecimal(Console.ReadLine());
+            var Sum = FirstNumber + SecondNumber;
+            Console.WriteLine(
+                String.Format("{0} + {1} = {2}",
+                FirstNumber, SecondNumber, Sum)
+                );
         }
     }
 }
